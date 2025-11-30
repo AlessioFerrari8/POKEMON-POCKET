895e967f3698024c6781f01c72f2d4a084540e12
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-11-30 14:34:45 +0100

    Feat: add component missing-cards

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Cards } from './cards/cards';
 import { Decks } from './decks/decks';
+import { MissingCards } from './missing-cards/missing-cards';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'cards', component: Cards },
   { path: 'decks', component: Decks },
+  { path: 'missing-cards', component: MissingCards },
   { path: '**', redirectTo: 'home' }
 ];
