d440651443c6d110feae9201dacfea5c74178b13
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-12-04 14:50:22 +0100

    Feat: add basic components to app + add routing between components

--- definitive/src/app/app.routes.ts
+++ definitive/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Decks } from './decks/decks';
+import { Cards } from './cards/cards';
+import { Wishlist } from './wishlist/wishlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', redirectTo: 'home', pathMatch: 'full'},
+    { path : 'home', component: Home },
+    { path : 'decks', component: Decks},
+    { path : 'cards', component: Cards},
+    { path : 'wishlist', component: Wishlist},
+    { path: '**', redirectTo: 'home' }, // url non definiti
+];
+
+
