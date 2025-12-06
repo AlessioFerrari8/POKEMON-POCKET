0bf73457267bec3f3f5c84b5c17b77f7a9df0cf0
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-12-06 14:46:18 +0100

    Feat: add nav-bar

--- definitive/src/app/app.ts
+++ definitive/src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavBar } from './nav-bar/nav-bar';
+import { Footer } from './footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('definitive');
+  title = 'Pokemon Pocket'
 }
